377da4c4cae69a8d1fd99ff75ce91bf9bf8d46f8
Federico Miotello <federico.miotello@mail.polimi.it>
2021-12-23 20:19:08 +0100

    Extension to n-dimensions + fixes

--- colorednoise.py
+++ colorednoise.py
@@ -2,17 +2,26 @@ import numpy as np
 
 def generate_noise(size, beta):
     white_noise = np.random.randn(*size)
-    white_noise_fft = np.fft.fft2(white)
+    white_noise_fft = np.fft.fftn(white_noise)
 
-    f_along_x = np.fft.fftfreq(size[0])
-    f_along_y = np.fft.fftfreq(size[1])
+    ndims = len(size)
+    freq_along_axis = []
 
-    grid1, grid2 = np.meshgrid(f_along_x, f_along_y)
-    freqs = np.sqrt(grid1**2 + grid2**2)
-    freqs[0,0] += 1e-8      # DC component
+    for axis in range(ndims):
+      freq_along_axis.append(np.fft.fftfreq(size[axis]))
+
+    grids = np.meshgrid(*freq_along_axis)
+    sum_of_squares = 0
+
+    for grid in grids:
+      sum_of_squares += grid**2
+
+    freqs = np.sqrt(sum_of_squares)
+    origin = (0,) * ndims
+    freqs[origin] += 1e-8      # DC component
     filter = 1/np.power(freqs, beta)
 
     colored_fft = white_fft * filter
-    color = np.fft.ifft2(colored_fft)
+    colored_noise = np.fft.ifftn(colored_fft)
 
-    return color
+    return np.abs(colored_noise)
